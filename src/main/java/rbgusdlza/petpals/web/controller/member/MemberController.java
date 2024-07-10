197978b39141cb840a58dd0bb26e34ddd10467a4
rbgusdlza <rbgusdlza@naver.com>
2024-07-10 18:43:04 +0900

    fix: html 파일 경로 수정 및 bootstrap 버전 수정

--- src/main/java/rbgusdlza/petpals/web/controller/member/MemberController.java
+++ src/main/java/rbgusdlza/petpals/web/controller/member/MemberController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
