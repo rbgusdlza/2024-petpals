0bbbc2274a1095617a06e905e60e867e3b1ea9e8
rbgusdlza <rbgusdlza@naver.com>
2024-07-11 19:14:01 +0900

    feat: MemberJoinServiceRequest dto 생성

--- src/main/java/rbgusdlza/petpals/web/service/member/MemberJoinServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/MemberJoinServiceRequest.java
@@ -1,2 +1,33 @@
-package rbgusdlza.petpals.web.service.member;public class MemberJoinServiceRequest {
+package rbgusdlza.petpals.web.service.member;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.member.Member;
+
+@Getter
+@NoArgsConstructor
+public class MemberJoinServiceRequest {
+
+    private String joinId;
+    private String nickname;
+    private String password;
+    private String email;
+
+    public Member toEntity() {
+        return Member.builder()
+                .loginId(joinId)
+                .nickname(nickname)
+                .password(password)
+                .email(email)
+                .build();
+    }
+
+    @Builder
+    private MemberJoinServiceRequest(String joinId, String nickname, String password, String email) {
+        this.joinId = joinId;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+    }
 }
