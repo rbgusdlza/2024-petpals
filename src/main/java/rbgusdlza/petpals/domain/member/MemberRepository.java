cb1098f577658a9bf711f4c9a63bd73e199812be
rbgusdlza <rbgusdlza@naver.com>
2024-07-11 19:10:32 +0900

    chore: 불필요한 import 정리

--- src/main/java/rbgusdlza/petpals/domain/member/MemberRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/member/MemberRepository.java
@@ -1,7 +1,6 @@
 package rbgusdlza.petpals.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import rbgusdlza.petpals.domain.member.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
