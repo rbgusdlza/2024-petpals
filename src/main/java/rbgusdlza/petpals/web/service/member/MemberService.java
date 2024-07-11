852cef1067a93e0e6383ef18ed15e66196c5e53f
rbgusdlza <rbgusdlza@naver.com>
2024-07-11 19:14:50 +0900

    feat: MemberService 생성 및 Member 객체를 저장하는 메서드 추가

--- src/main/java/rbgusdlza/petpals/web/service/member/MemberService.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/MemberService.java
@@ -1,2 +1,21 @@
-package rbgusdlza.petpals.web.service.member;public class MemberService {
+package rbgusdlza.petpals.web.service.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import rbgusdlza.petpals.domain.member.Member;
+import rbgusdlza.petpals.domain.member.MemberRepository;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void joinMember(MemberJoinServiceRequest request) {
+        Member member = request.toEntity();
+        memberRepository.save(member);
+    }
 }
