512716d959d8197fe2dbaf07d70670a750fff66a
rbgusdlza <rbgusdlza@naver.com>
2024-07-12 20:42:03 +0900

    feat: 도메인 영역 클래스 생성

---
+++ src/main/java/rbgusdlza/petpals/domain/error/PetPalsException.java
@@ -0,0 +1,8 @@
+package rbgusdlza.petpals.domain.error;
+
+public class PetPalsException extends RuntimeException {
+
+    public PetPalsException(String message) {
+        super(message);
+    }
+}
