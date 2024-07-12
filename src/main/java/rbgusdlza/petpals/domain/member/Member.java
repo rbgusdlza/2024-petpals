fb7b8ff5f93bf4e8235d0dc83e751044da51b433
rbgusdlza <rbgusdlza@naver.com>
2024-07-12 22:40:03 +0900

    refactor: Member 객체 검증 로직 dto로 이동

--- src/main/java/rbgusdlza/petpals/domain/member/Member.java
+++ src/main/java/rbgusdlza/petpals/domain/member/Member.java
@@ -6,15 +6,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import rbgusdlza.petpals.domain.BaseEntity;
-import rbgusdlza.petpals.domain.error.PetPalsException;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member extends BaseEntity {
-
-    public static final int MINIMUM_LOGIN_ID_LENGTH = 8;
-    public static final int MAXIMUM_LOGIN_ID_LENGTH = 15;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,39 +23,12 @@ public class Member extends BaseEntity {
     private String email;
 
     public static Member of(String loginId, String nickname, String password, String email) {
-        Member member = Member.builder()
+        return Member.builder()
                 .loginId(loginId)
                 .nickname(nickname)
                 .password(password)
                 .email(email)
                 .build();
-        member.checkIfMemberIsValid();
-        return member;
-    }
-
-    private void checkIfMemberIsValid() {
-        checkIfLoginIdIsValid();
-//        checkIfNicknameIsValid(nickname);
-//        checkIfPasswordIsValid(password);
-//        checkIfEmailIsValid(email);
-    }
-
-    private void checkIfLoginIdIsValid() {
-        if (isInvalidLoginIdLength(loginId)) {
-            throw new PetPalsException("아이디는 8자에서 15자 사이여야 합니다.");
-        }
-
-        if (isInvalidLoginIdFormat(loginId)) {
-            throw new PetPalsException("아이디는 영어 대문자, 소문자, 숫자로만 구성되어야 합니다.");
-        }
-    }
-
-    private boolean isInvalidLoginIdFormat(String loginId) {
-        return !loginId.matches("[a-zA-Z0-9]+");
-    }
-
-    private boolean isInvalidLoginIdLength(String loginId) {
-        return loginId.length() < MINIMUM_LOGIN_ID_LENGTH || loginId.length() > MAXIMUM_LOGIN_ID_LENGTH;
     }
 
     @Builder
--- src/main/java/rbgusdlza/petpals/web/service/member/MemberJoinServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/MemberJoinServiceRequest.java
@@ -3,29 +3,59 @@ package rbgusdlza.petpals.web.service.member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.error.PetPalsException;
 import rbgusdlza.petpals.domain.member.Member;
 
 @Getter
 @NoArgsConstructor
 public class MemberJoinServiceRequest {
 
-    private String joinId;
+    public static final int MINIMUM_LOGIN_ID_LENGTH = 8;
+    public static final int MAXIMUM_LOGIN_ID_LENGTH = 15;
+
+    private String loginId;
     private String nickname;
     private String password;
     private String email;
 
     public Member toEntity() {
+        checkIfMemberInputIsValid();
         return Member.builder()
-                .loginId(joinId)
+                .loginId(loginId)
                 .nickname(nickname)
                 .password(password)
                 .email(email)
                 .build();
     }
 
+    private void checkIfMemberInputIsValid() {
+        checkIfLoginIdIsValid();
+//        checkIfNicknameIsValid(nickname);
+//        checkIfPasswordIsValid(password);
+//        checkIfEmailIsValid(email);
+    }
+
+    private void checkIfLoginIdIsValid() {
+        if (isInvalidLoginIdLength(loginId)) {
+            throw new PetPalsException("아이디는 8자에서 15자 사이여야 합니다.");
+        }
+
+        if (isInvalidLoginIdFormat(loginId)) {
+            throw new PetPalsException("아이디는 영어 대문자, 소문자, 숫자로만 구성되어야 합니다.");
+        }
+    }
+
+    private boolean isInvalidLoginIdFormat(String loginId) {
+        return !loginId.matches("[a-zA-Z0-9]+");
+    }
+
+    private boolean isInvalidLoginIdLength(String loginId) {
+        return loginId.length() < MINIMUM_LOGIN_ID_LENGTH || loginId.length() > MAXIMUM_LOGIN_ID_LENGTH;
+    }
+
     @Builder
     private MemberJoinServiceRequest(String joinId, String nickname, String password, String email) {
-        this.joinId = joinId;
+        this.loginId = joinId;
         this.nickname = nickname;
         this.password = password;
         this.email = email;
