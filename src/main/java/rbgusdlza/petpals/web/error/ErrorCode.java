2a01b63fb173a9f64a9058d13ae8497d3d6b363b
rbgusdlza <rbgusdlza@naver.com>
2024-07-13 23:08:38 +0900

    refactor: 사용자 등록 검증 로직 MemberJoinRequest로 이동

--- src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
+++ src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    INVALID_LOGIN_ID_LENGTH("로그인 아이디 길이 오류", "아이디는 8자에서 15자 사이여야 합니다."),
-    INVALID_LOGIN_ID_FORMAT("로그인 아이디 포맷 오류", "아이디는 영어 대문자, 소문자, 숫자로만 구성되어야 합니다."),
     ;
-
 
     private final String description;
     private final String message;
--- src/main/java/rbgusdlza/petpals/web/service/member/request/MemberJoinServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/request/MemberJoinServiceRequest.java
@@ -3,15 +3,11 @@ package rbgusdlza.petpals.web.service.member.request;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import rbgusdlza.petpals.web.error.PetPalsException;
 import rbgusdlza.petpals.domain.member.Member;
 
 @Getter
 @NoArgsConstructor
 public class MemberJoinServiceRequest {
-
-    public static final int MINIMUM_LOGIN_ID_LENGTH = 8;
-    public static final int MAXIMUM_LOGIN_ID_LENGTH = 15;
 
     private String loginId;
     private String nickname;
@@ -19,38 +15,12 @@ public class MemberJoinServiceRequest {
     private String email;
 
     public Member toEntity() {
-        checkIfMemberInputIsValid();
         return Member.builder()
                 .loginId(loginId)
                 .nickname(nickname)
                 .password(password)
                 .email(email)
                 .build();
-    }
-
-    private void checkIfMemberInputIsValid() {
-        checkIfLoginIdIsValid();
-//        checkIfNicknameIsValid(nickname);
-//        checkIfPasswordIsValid(password);
-//        checkIfEmailIsValid(email);
-    }
-
-    private void checkIfLoginIdIsValid() {
-        if (isInvalidLoginIdLength(loginId)) {
-            throw new PetPalsException("아이디는 8자에서 15자 사이여야 합니다.");
-        }
-
-        if (isInvalidLoginIdFormat(loginId)) {
-            throw new PetPalsException("아이디는 영어 대문자, 소문자, 숫자로만 구성되어야 합니다.");
-        }
-    }
-
-    private boolean isInvalidLoginIdFormat(String loginId) {
-        return !loginId.matches("[a-zA-Z0-9]+");
-    }
-
-    private boolean isInvalidLoginIdLength(String loginId) {
-        return loginId.length() < MINIMUM_LOGIN_ID_LENGTH || loginId.length() > MAXIMUM_LOGIN_ID_LENGTH;
     }
 
     @Builder
