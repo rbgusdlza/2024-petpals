813b9e027c080ca0e68dd571d5bf8474f1f9fdb3
rbgusdlza <rbgusdlza@naver.com>
2024-07-13 20:35:34 +0900

    feat: PetPalsException에 errorCode 필드 추가

--- src/main/java/rbgusdlza/petpals/web/error/PetPalsException.java
+++ src/main/java/rbgusdlza/petpals/web/error/PetPalsException.java
@@ -1,8 +1,12 @@
 package rbgusdlza.petpals.web.error;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class PetPalsException extends RuntimeException {
 
-    public PetPalsException(String message) {
-        super(message);
-    }
+    private final ErrorCode errorCode;
+
 }
