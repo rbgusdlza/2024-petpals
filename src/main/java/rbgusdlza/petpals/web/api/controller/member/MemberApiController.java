261b0ec9f686a926d3443d4ce7658a72d392a7a9
rbgusdlza <rbgusdlza@naver.com>
2024-07-15 16:31:00 +0900

    feat: api 관련 dto, controller 추가

--- src/main/java/rbgusdlza/petpals/web/api/controller/member/MemberApiController.java
+++ src/main/java/rbgusdlza/petpals/web/api/controller/member/MemberApiController.java
@@ -1,4 +1,32 @@
-package rbgusdlza.petpals.web.api.controller;
+package rbgusdlza.petpals.web.api.controller.member;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rbgusdlza.petpals.web.api.ApiResponse;
+import rbgusdlza.petpals.web.api.controller.member.request.LoginIdForm;
+import rbgusdlza.petpals.web.service.member.MemberService;
+import rbgusdlza.petpals.web.service.member.response.LoginIdCheckResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+@RestController
 public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/check-login-id")
+    public ApiResponse<LoginIdCheckResponse> checkLoginId(@Valid @RequestBody LoginIdForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ApiResponse.of(HttpStatus.BAD_REQUEST, "Invalid Login ID", null);
+        }
+        return ApiResponse.ok(memberService.isLoginIdDuplicate(form.toServiceForm()));
+    }
 }
--- src/main/java/rbgusdlza/petpals/web/api/controller/member/request/EmailForm.java
+++ src/main/java/rbgusdlza/petpals/web/api/controller/member/request/EmailForm.java
@@ -1,10 +1,27 @@
-package rbgusdlza.petpals.web.api.controller.request;
+package rbgusdlza.petpals.web.api.controller.member.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import rbgusdlza.petpals.web.service.member.request.EmailServiceForm;
 
 @Getter @Setter
+@NoArgsConstructor
 public class EmailForm {
 
+    @NotBlank
     private String email;
+
+    public EmailServiceForm toServiceForm() {
+        return EmailServiceForm.builder()
+                .email(email)
+                .build();
+    }
+
+    @Builder
+    private EmailForm(String email) {
+        this.email = email;
+    }
 }
