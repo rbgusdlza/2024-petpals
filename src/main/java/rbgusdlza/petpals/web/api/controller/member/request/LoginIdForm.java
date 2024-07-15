9eaf8c5c41648f1d3c768f46d4d48346b045fd14
rbgusdlza <rbgusdlza@naver.com>
2024-07-15 16:31:36 +0900

    feat: api dto 추가

--- src/main/java/rbgusdlza/petpals/web/api/controller/member/request/LoginIdForm.java
+++ src/main/java/rbgusdlza/petpals/web/api/controller/member/request/LoginIdForm.java
@@ -1,10 +1,27 @@
-package rbgusdlza.petpals.web.api.controller.request;
+package rbgusdlza.petpals.web.api.controller.member.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import rbgusdlza.petpals.web.service.member.request.LoginIdServiceForm;
 
 @Getter @Setter
+@NoArgsConstructor
 public class LoginIdForm {
 
+    @NotBlank
     private String loginId;
+
+    public LoginIdServiceForm toServiceForm() {
+        return LoginIdServiceForm.builder()
+                .loginId(loginId)
+                .build();
+    }
+
+    @Builder
+    private LoginIdForm(String loginId) {
+        this.loginId = loginId;
+    }
 }
