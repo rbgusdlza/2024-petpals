010aed45c193722873781d3298f5111f71dfd23b
rbgusdlza <rbgusdlza@naver.com>
2024-07-15 16:35:44 +0900

    feat: service dto 추가

--- src/main/java/rbgusdlza/petpals/web/service/member/request/EmailServiceForm.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/request/EmailServiceForm.java
@@ -1,2 +1,17 @@
-package rbgusdlza.petpals.web.service.member.request;public class EmailServiceForm {
+package rbgusdlza.petpals.web.service.member.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailServiceForm {
+
+    private String email;
+
+    @Builder
+    private EmailServiceForm(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/rbgusdlza/petpals/web/service/member/request/LoginIdServiceForm.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/request/LoginIdServiceForm.java
@@ -1,2 +1,17 @@
-package rbgusdlza.petpals.web.service.member.request;public class LoginIdServiceForm {
+package rbgusdlza.petpals.web.service.member.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginIdServiceForm {
+
+    private String loginId;
+
+    @Builder
+    private LoginIdServiceForm(String loginId) {
+        this.loginId = loginId;
+    }
 }
