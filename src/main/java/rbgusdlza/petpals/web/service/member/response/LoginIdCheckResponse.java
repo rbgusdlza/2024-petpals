5ebd372174f679b625298efcdfa8910e9d8d0e81
rbgusdlza <rbgusdlza@naver.com>
2024-07-15 16:35:15 +0900

    feat: LoginIdCheckResponse dto 추가

--- src/main/java/rbgusdlza/petpals/web/service/member/response/LoginIdCheckResponse.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/response/LoginIdCheckResponse.java
@@ -1,9 +1,21 @@
 package rbgusdlza.petpals.web.service.member.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class LoginIdResponse {
+public class LoginIdCheckResponse {
 
-    
+    private Boolean isDuplicated;
+
+    public static LoginIdCheckResponse from(Boolean isDuplicated) {
+        return LoginIdCheckResponse.builder()
+                .isDuplicated(isDuplicated)
+                .build();
+    }
+
+    @Builder
+    private LoginIdCheckResponse(Boolean isDuplicated) {
+        this.isDuplicated = isDuplicated;
+    }
 }
