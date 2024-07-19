5b3f1b345e6f9cfc7e994761d03fd02d914f4f74
rbgusdlza <rbgusdlza@naver.com>
2024-07-19 19:38:28 +0900

    feat: 비밀번호 암호화하는 유틸 클래스 생성

--- src/main/java/rbgusdlza/petpals/global/util/PasswordEncryptor.java
+++ src/main/java/rbgusdlza/petpals/global/util/PasswordEncryptor.java
@@ -1,2 +1,19 @@
-package rbgusdlza.petpals.global.util;public class PasswordEncryptor {
+package rbgusdlza.petpals.global.util;
+
+import com.google.common.hash.Hashing;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.nio.charset.StandardCharsets;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class PasswordEncryptor {
+
+    private static final String SALT = "kyuzzang";
+
+    public static String encryptPasswordFrom(String password) {
+        return Hashing.sha256()
+                .hashString(password + SALT, StandardCharsets.UTF_8)
+                .toString();
+    }
 }
