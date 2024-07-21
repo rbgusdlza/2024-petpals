4e78f68a6ee370ba5b3cc865a5991076fb07edf1
rbgusdlza <rbgusdlza@naver.com>
2024-07-21 18:39:23 +0900

    feat: 에러 코드 추가

--- src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
+++ src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
@@ -7,8 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
+    DUPLICATE_LOGIN_ID("중복된 아이디"),
+    DUPLICATE_NICKNAME("중복된 닉네임"),
     ;
 
     private final String description;
-    private final String message;
 }
