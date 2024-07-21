f474bad7b17c9912e88d4d8e21f92ca09db35299
rbgusdlza <rbgusdlza@naver.com>
2024-07-21 18:40:57 +0900

    feat: Member 저장하는 메서드에 에러 조건 추가

--- src/main/java/rbgusdlza/petpals/web/service/member/MemberService.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/MemberService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rbgusdlza.petpals.domain.member.Member;
 import rbgusdlza.petpals.domain.member.MemberRepository;
+import rbgusdlza.petpals.web.error.ErrorCode;
+import rbgusdlza.petpals.web.error.PetPalsException;
 import rbgusdlza.petpals.web.service.member.request.LoginIdServiceForm;
 import rbgusdlza.petpals.web.service.member.request.MemberJoinServiceRequest;
 import rbgusdlza.petpals.web.service.member.request.NicknameServiceForm;
@@ -20,6 +22,9 @@ public class MemberService {
 
     @Transactional
     public void joinMember(MemberJoinServiceRequest request) {
+        checkIfLoginIdDuplicate(request);
+        checkIfNicknameDuplicate(request);
+
         Member member = request.toEntity();
         memberRepository.save(member);
     }
@@ -34,6 +39,22 @@ public class MemberService {
         String nickname = form.getNickname();
         Member findMember = memberRepository.findByNickname(nickname);
         return NicknameCheckResponse.from(isDuplicated(findMember));
+    }
+
+    private void checkIfLoginIdDuplicate(MemberJoinServiceRequest request) {
+        String loginId = request.getLoginId();
+        Member findMember = memberRepository.findByLoginId(loginId);
+        if (isDuplicated(findMember)) {
+            throw new PetPalsException(ErrorCode.DUPLICATE_LOGIN_ID);
+        }
+    }
+
+    private void checkIfNicknameDuplicate(MemberJoinServiceRequest request) {
+        String nickname = request.getNickname();
+        Member findMember = memberRepository.findByNickname(nickname);
+        if (isDuplicated(findMember)) {
+            throw new PetPalsException(ErrorCode.DUPLICATE_NICKNAME);
+        }
     }
 
     private boolean isDuplicated(Member findMember) {
