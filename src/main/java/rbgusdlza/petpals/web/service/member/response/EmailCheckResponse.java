dede2814b4f88fd5b6d3b1967872cc22cb0cd07c
rbgusdlza <rbgusdlza@naver.com>
2024-07-22 16:47:55 +0900

    feat: 이메일 중복 확인 response 추가

--- src/main/java/rbgusdlza/petpals/web/service/member/response/EmailCheckResponse.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/response/EmailCheckResponse.java
@@ -1,2 +1,21 @@
-package rbgusdlza.petpals.web.service.member.response;public class EmailCheckResponse {
+package rbgusdlza.petpals.web.service.member.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class EmailCheckResponse {
+
+    private Boolean isDuplicated;
+
+    @Builder
+    private EmailCheckResponse(boolean isDuplicated) {
+        this.isDuplicated = isDuplicated;
+    }
+
+    public static EmailCheckResponse from(boolean isDuplicated) {
+        return EmailCheckResponse.builder()
+                .isDuplicated(isDuplicated)
+                .build();
+    }
 }
