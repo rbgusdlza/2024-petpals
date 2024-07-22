1c31a5e1184de3d520eb3c0602babdaee05cdd22
rbgusdlza <rbgusdlza@naver.com>
2024-07-22 17:53:24 +0900

    feat: MemberResponse 추가

--- src/main/java/rbgusdlza/petpals/web/service/member/response/MemberResponse.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/response/MemberResponse.java
@@ -1,2 +1,31 @@
-package rbgusdlza.petpals.web.service.member.response;public class MemberResponse {
+package rbgusdlza.petpals.web.service.member.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import rbgusdlza.petpals.domain.member.Member;
+
+@Getter
+public class MemberResponse {
+
+    private Long id;
+    private String loginId;
+    private String nickname;
+    private String email;
+
+    @Builder
+    private MemberResponse(Long id, String loginId, String nickname, String email) {
+        this.id = id;
+        this.loginId = loginId;
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+    public static MemberResponse of(Member member) {
+        return MemberResponse.builder()
+                .id(member.getId())
+                .loginId(member.getLoginId())
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .build();
+    }
 }
