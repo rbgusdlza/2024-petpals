4e5fca8e5f4ca6b7ecf8194702b870069d7b8769
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 20:21:35 +0900

    feat: 빌더 및 서비스 request 형식으로 변경하는 메서드 추가

--- src/main/java/rbgusdlza/petpals/web/controller/member/request/MemberLoginRequest.java
+++ src/main/java/rbgusdlza/petpals/web/controller/member/request/MemberLoginRequest.java
@@ -1,17 +1,34 @@
 package rbgusdlza.petpals.web.controller.member.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import rbgusdlza.petpals.global.util.PasswordEncryptor;
+import rbgusdlza.petpals.web.service.member.request.MemberLoginServiceRequest;
 
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 public class MemberLoginRequest {
 
-    @NotBlank
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String loginId;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
+    @Builder
+    private MemberLoginRequest(String loginId, String password) {
+        this.loginId = loginId;
+        this.password = password;
+    }
+
+    public MemberLoginServiceRequest toServiceRequest() {
+        String encryptedPassword = PasswordEncryptor.encryptPasswordFrom(password);
+        return MemberLoginServiceRequest.builder()
+                .loginId(loginId)
+                .encryptedPassword(encryptedPassword)
+                .build();
+    }
 }
