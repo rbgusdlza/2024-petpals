7a0636c10d56562a850d3ee8e5374d1c2bb4220b
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 20:45:38 +0900

    test: ActiveProfiles을 test로 설정

--- src/test/java/rbgusdlza/petpals/domain/member/MemberRepositoryTest.java
+++ src/test/java/rbgusdlza/petpals/domain/member/MemberRepositoryTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MemberRepositoryTest {
 
