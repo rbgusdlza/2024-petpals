c0ea75229a080c0f1cb09673fa170d65b91bf43b
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 20:20:31 +0900

    feat: MemberLoginServiceRequest 생성

--- src/main/java/rbgusdlza/petpals/web/service/member/request/MemberLoginServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/member/request/MemberLoginServiceRequest.java
@@ -1,2 +1,26 @@
-package rbgusdlza.petpals.web.service.member.request;public class MemberLoginServiceRequest {
+package rbgusdlza.petpals.web.service.member.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberLoginServiceRequest {
+
+    private String loginId;
+    private String encryptedPassword;
+
+    @Builder
+    private MemberLoginServiceRequest(String loginId, String encryptedPassword) {
+        this.loginId = loginId;
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public MemberLoginServiceRequest of(String loginId, String encryptedPassword) {
+        return MemberLoginServiceRequest.builder()
+                .loginId(loginId)
+                .encryptedPassword(encryptedPassword)
+                .build();
+    }
 }
