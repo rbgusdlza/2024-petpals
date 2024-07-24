a6b6a903f314ef9720dcf30ee57c287d039a3b42
rbgusdlza <rbgusdlza@naver.com>
2024-07-25 00:51:30 +0900

    test: isLoginIdDuplicate 메서드 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/service/member/MemberServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/member/MemberServiceTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import rbgusdlza.petpals.domain.member.Member;
 import rbgusdlza.petpals.domain.member.MemberRepository;
+import rbgusdlza.petpals.web.service.member.request.LoginIdServiceForm;
 import rbgusdlza.petpals.web.service.member.request.MemberJoinServiceRequest;
 import rbgusdlza.petpals.web.service.member.request.MemberLoginServiceRequest;
+import rbgusdlza.petpals.web.service.member.response.LoginIdCheckResponse;
 import rbgusdlza.petpals.web.service.member.response.MemberResponse;
 
 import static org.assertj.core.api.Assertions.*;
@@ -77,5 +79,37 @@ class MemberServiceTest {
 
         //then
         assertThat(response).isNull();
+    }
+
+    @DisplayName("로그인 아이디가 중복되었는지 확인한다.")
+    @Test
+    void isLoginIdDuplicate() {
+        //given
+        Member member = Member.of("userA", "park", "1234", "member@gmail.com");
+        memberRepository.save(member);
+
+        LoginIdServiceForm form = LoginIdServiceForm.of("userA");
+
+        //when
+        LoginIdCheckResponse response = memberService.isLoginIdDuplicate(form);
+
+        //then
+        assertThat(response.getIsDuplicated()).isTrue();
+    }
+
+    @DisplayName("로그인 아이디가 중복되었는지 확인한다.")
+    @Test
+    void isLoginIdDuplicate2() {
+        //given
+        Member member = Member.of("userA", "park", "1234", "member@gmail.com");
+        memberRepository.save(member);
+
+        LoginIdServiceForm form = LoginIdServiceForm.of("userB");
+
+        //when
+        LoginIdCheckResponse response = memberService.isLoginIdDuplicate(form);
+
+        //then
+        assertThat(response.getIsDuplicated()).isFalse();
     }
 }
