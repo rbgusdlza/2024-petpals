edda527d224d9058d50fc743e1efdb3aeac119b0
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 21:00:13 +0900

    feat: 로그인 인터셉터 기능 추가

--- src/main/java/rbgusdlza/petpals/global/interceptor/LoginCheckInterceptor.java
+++ src/main/java/rbgusdlza/petpals/global/interceptor/LoginCheckInterceptor.java
@@ -1,8 +1,25 @@
 package rbgusdlza.petpals.global.interceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        HttpSession session = request.getSession(false);
 
+        if (doesUserLogin(session)) {
+            response.sendRedirect("/member/login?redirectURL=" + requestURI);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean doesUserLogin(HttpSession session) {
+        return session == null || session.getAttribute("id") == null;
+    }
 }
