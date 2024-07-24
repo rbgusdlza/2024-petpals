21b19a85dd1289a83f204a052603b39014672608
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 20:59:57 +0900

    feat: 인터셉터 추가

--- src/main/java/rbgusdlza/petpals/global/config/WebConfig.java
+++ src/main/java/rbgusdlza/petpals/global/config/WebConfig.java
@@ -1,4 +1,21 @@
 package rbgusdlza.petpals.global.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import rbgusdlza.petpals.global.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/member/join", "/member/login",
+                        "/css/**", "/js/**", "/error"
+                );
+    }
 }
