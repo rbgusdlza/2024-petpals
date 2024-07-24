5fac2987774e9b25a5295277591e657e16647434
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 20:23:15 +0900

    feat: 로그인 메서드 구현

--- src/main/java/rbgusdlza/petpals/web/controller/member/MemberController.java
+++ src/main/java/rbgusdlza/petpals/web/controller/member/MemberController.java
@@ -1,5 +1,7 @@
 package rbgusdlza.petpals.web.controller.member;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rbgusdlza.petpals.domain.member.Member;
 import rbgusdlza.petpals.web.controller.member.request.MemberJoinRequest;
+import rbgusdlza.petpals.web.controller.member.request.MemberLoginRequest;
 import rbgusdlza.petpals.web.service.member.MemberService;
 
 @Slf4j
@@ -22,7 +26,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/join")
-    public String joinForm(Model model) {
+    public String join(Model model) {
         model.addAttribute("memberJoinRequest", new MemberJoinRequest());
         return "member/join";
     }
@@ -33,13 +37,34 @@ public class MemberController {
             log.info("bindingResult : {}", bindingResult);
             return "member/join";
         }
-        memberService.joinMember(request.toServiceRequest());
+        memberService.join(request.toServiceRequest());
         return "redirect:/";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("memberLoginRequest", new MemberLoginRequest());
         return "member/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute MemberLoginRequest request,
+                        BindingResult bindingResult,
+                        HttpSession session) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult : {}", bindingResult);
+            return "member/login";
+        }
+
+        Member loginMember = memberService.login(request.toServiceRequest());
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "member/login";
+        }
+
+        session.setAttribute("id", loginMember.getId());
+        return "redirect:/";
     }
 }
 
