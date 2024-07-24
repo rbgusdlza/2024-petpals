f181fbc6a7f4223ce06dbaae962a514b3f98273c
rbgusdlza <rbgusdlza@naver.com>
2024-07-24 19:02:39 +0900

    refactor: 조건 필드에 있는 메시지 제거

--- src/main/java/rbgusdlza/petpals/web/controller/member/request/MemberLoginRequest.java
+++ src/main/java/rbgusdlza/petpals/web/controller/member/request/MemberLoginRequest.java
@@ -8,10 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberLoginRequest {
 
-    @NotBlank(message = "아이디를 입력하세요.")
+    @NotBlank
     private String loginId;
 
-    @NotBlank(message = "비밀번호를 입력하세요.")
+    @NotBlank
     private String password;
 
 }
