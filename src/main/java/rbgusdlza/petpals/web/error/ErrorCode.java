1965287becd48a714128ee810c17437e22048eaa
rbgusdlza <rbgusdlza@naver.com>
2024-07-25 22:13:07 +0900

    feat: 회원가입에 대한 에러 코드 추가

--- src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
+++ src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     DUPLICATE_NICKNAME("중복된 닉네임"),
     DUPLICATE_EMAIL("중복된 이메일"),
     SENDING_EMAIL_ERROR("이메일 전송 실패"),
-    MEMBER_LOGIN_ERROR("매칭되는 사용자 정보 없음")
+    MEMBER_LOGIN_ERROR("매칭되는 사용자 정보 없음"),
+    MEMBER_JOIN_ERROR("입력값과 중복된 사용자가 이미 존재"),
     ;
 
     private final String description;
