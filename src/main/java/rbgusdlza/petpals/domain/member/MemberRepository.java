0e9cff147567ea060e4cb33a6eea75f992d76ca8
rbgusdlza <rbgusdlza@naver.com>
2024-07-25 22:12:09 +0900

    feat: 로그인 아이디, 닉네임, 이메일 중 하나라도 일치하는 사용자의 수를 반환하는 메서드 추가

--- src/main/java/rbgusdlza/petpals/domain/member/MemberRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package rbgusdlza.petpals.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +16,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByEmail(String email);
 
     Optional<Member> findByLoginIdAndEncryptedPassword(String loginId, String encryptedPassword);
+
+    @Query("SELECT COUNT(m) FROM Member m WHERE m.loginId = :loginId OR m.nickname = :nickname OR m.email = :email")
+    long countByLoginIdOrNicknameOrEmail(@Param("loginId") String loginId, @Param("nickname") String nickname, @Param("email") String email);
 }
