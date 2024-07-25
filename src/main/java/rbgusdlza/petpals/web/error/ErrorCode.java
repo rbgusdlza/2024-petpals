a539a3dd0164232f592a301fe93b85506a2c2376
rbgusdlza <rbgusdlza@naver.com>
2024-07-25 18:33:18 +0900

    feat: MEMBER_LOGIN_ERROR 추가

--- src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
+++ src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     DUPLICATE_NICKNAME("중복된 닉네임"),
     DUPLICATE_EMAIL("중복된 이메일"),
     SENDING_EMAIL_ERROR("이메일 전송 실패"),
+    MEMBER_LOGIN_ERROR("매칭되는 사용자 정보 없음")
     ;
 
     private final String description;
