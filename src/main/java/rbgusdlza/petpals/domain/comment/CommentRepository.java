f093ba23039a672fbfe17fb85ecfba5ad776403e
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 20:07:34 +0900

    feat: CommentRepository 생성

--- src/main/java/rbgusdlza/petpals/domain/comment/CommentRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/comment/CommentRepository.java
@@ -2,7 +2,10 @@ package rbgusdlza.petpals.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findAllByPostId(Long postId);
 
 }
