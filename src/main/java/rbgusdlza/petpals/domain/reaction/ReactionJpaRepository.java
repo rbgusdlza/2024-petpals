06309551a49225f7eb7764f4c62e29cffe4d6256
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 18:43:41 +0900

    feat: ReactionJpaRepository 생성 및 메서드 선언

--- src/main/java/rbgusdlza/petpals/domain/reaction/ReactionJpaRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/reaction/ReactionJpaRepository.java
@@ -1,2 +1,14 @@
-package rbgusdlza.petpals.domain.reaction;public class ReactionJpaRepository {
+package rbgusdlza.petpals.domain.reaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rbgusdlza.petpals.domain.EntityStatus;
+
+import java.util.Optional;
+
+public interface ReactionJpaRepository extends JpaRepository<Reaction, Long> {
+
+    Optional<Reaction> findByMemberIdAndTargetIdAndTargetTypeAndType(Long memberId, Long targetId, TargetType targetType, ReactionType type);
+
+    long countByTargetIdAndTargetTypeAndTypeAndEntityStatus(Long targetId, TargetType targetType, ReactionType type, EntityStatus entityStatus);
+
 }
