b794f225afbfce2250e7b07017ae8fe18a02f6d5
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 20:08:38 +0900

    refactor: 메서드 이름 변경

--- src/main/java/rbgusdlza/petpals/domain/reaction/ReactionJpaRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/reaction/ReactionJpaRepository.java
@@ -3,11 +3,12 @@ package rbgusdlza.petpals.domain.reaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rbgusdlza.petpals.domain.EntityStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReactionJpaRepository extends JpaRepository<Reaction, Long> {
 
-    Optional<Reaction> findByMemberIdAndTargetIdAndTargetTypeAndType(Long memberId, Long targetId, TargetType targetType, ReactionType type);
+    List<Reaction> findAllByMemberIdAndTargetIdAndTargetTypeAndType(Long memberId, Long targetId, TargetType targetType, ReactionType type);
 
     long countByTargetIdAndTargetTypeAndTypeAndEntityStatus(Long targetId, TargetType targetType, ReactionType type, EntityStatus entityStatus);
 
