8ae57c777b56565cc8b4f2aae70f7323a1995533
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 18:44:36 +0900

    feat: 사진 관련 엔티티 생성

--- src/main/java/rbgusdlza/petpals/domain/photo/Photo.java
+++ src/main/java/rbgusdlza/petpals/domain/photo/Photo.java
@@ -1,2 +1,20 @@
-package rbgusdlza.petpals.domain.photo;public class Photo {
+package rbgusdlza.petpals.domain.photo;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Photo extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id")
+    private Long id;
+
+    private Long postId;
 }
