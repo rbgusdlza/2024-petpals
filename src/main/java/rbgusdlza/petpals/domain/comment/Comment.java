7855dbc291253467f00998649e235899057c2838
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 18:45:01 +0900

    feat: 댓글 관련 엔티티 생성

--- src/main/java/rbgusdlza/petpals/domain/comment/Comment.java
+++ src/main/java/rbgusdlza/petpals/domain/comment/Comment.java
@@ -1,2 +1,21 @@
-package rbgusdlza.petpals.domain.comment;public class Comment {
+package rbgusdlza.petpals.domain.comment;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private Long postId;
+    private String content;
 }
