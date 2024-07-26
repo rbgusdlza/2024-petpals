6e6b62b078280a9764f27398728b2f11d9e4766c
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 10:24:29 +0900

    feat: controllerAdvice 생성 및 구현

--- src/main/java/rbgusdlza/petpals/web/WebControllerAdvice.java
+++ src/main/java/rbgusdlza/petpals/web/WebControllerAdvice.java
@@ -1,7 +1,19 @@
 package rbgusdlza.petpals.web;
 
-@org.springframework.web.bind.annotation.ControllerAdvice
-public class ControllerAdvice {
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import rbgusdlza.petpals.web.error.PetPalsException;
 
+@ControllerAdvice
+public class WebControllerAdvice {
 
+    @ExceptionHandler(PetPalsException.class)
+    public ModelAndView handlePetPalsException(PetPalsException exception, Model model) {
+        ModelAndView modelAndView = new ModelAndView("error/error");
+        model.addAttribute("errorCode", exception.getErrorCode());
+        model.addAttribute("errorMessage", exception.getErrorCode().getDescription());
+        return modelAndView;
+    }
 }
