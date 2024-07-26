bca09108ba29101d0917eb1d66c14a00c4a84cdb
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 18:45:19 +0900

    feat: 인기도 관련 엔티티 생성

--- src/main/java/rbgusdlza/petpals/domain/popularity/Popularity.java
+++ src/main/java/rbgusdlza/petpals/domain/popularity/Popularity.java
@@ -1,2 +1,22 @@
-package rbgusdlza.petpals.domain.popularity;public class Popularity {
+package rbgusdlza.petpals.domain.popularity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Popularity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "popularity_id")
+    private Long id;
+
+    private Long postId;
+    private int score;
+
 }
