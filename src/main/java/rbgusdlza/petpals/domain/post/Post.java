261c0d39ca7769c40c34d60b264c374c7b0a55bc
rbgusdlza <rbgusdlza@naver.com>
2024-07-26 18:44:07 +0900

    feat: 게시물 관련 엔티티 생성

--- src/main/java/rbgusdlza/petpals/domain/post/Post.java
+++ src/main/java/rbgusdlza/petpals/domain/post/Post.java
@@ -1,2 +1,21 @@
-package rbgusdlza.petpals.domain.post;public class Post {
+package rbgusdlza.petpals.domain.post;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private Long memberId;
+    private String content;
 }
