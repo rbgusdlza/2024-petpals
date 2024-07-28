d15c159936a554b78f8f8c9d0d0b84a8401a2412
rbgusdlza <rbgusdlza@naver.com>
2024-07-28 20:51:57 +0900

    feat: PopularityRepository 생성

---
+++ src/main/java/rbgusdlza/petpals/domain/popularity/PopularityRepository.java
@@ -0,0 +1,6 @@
+package rbgusdlza.petpals.domain.popularity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopularityRepository extends JpaRepository<Popularity, Long> {
+}
