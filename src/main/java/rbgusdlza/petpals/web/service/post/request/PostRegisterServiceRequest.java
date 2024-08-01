4e461bc38cf520a994b19884fe7ef9e0edd8851e
rbgusdlza <rbgusdlza@naver.com>
2024-08-01 16:51:14 +0900

    feat: PostRegisterServiceRequest 생성

--- src/main/java/rbgusdlza/petpals/web/service/post/request/PostRegisterServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/post/request/PostRegisterServiceRequest.java
@@ -1,2 +1,26 @@
-package rbgusdlza.petpals.web.service.post.request;public class PostRegisterServiceRequest {
+package rbgusdlza.petpals.web.service.post.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rbgusdlza.petpals.domain.post.Post;
+
+@Getter
+@NoArgsConstructor
+public class PostRegisterServiceRequest {
+
+    private Long memberId;
+    private String title;
+    private String content;
+
+    @Builder
+    private PostRegisterServiceRequest(Long memberId, String title, String content) {
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Post toEntity() {
+        return Post.of(memberId, title, content);
+    }
 }
