7ca8a1d8ccb064aec3f52b0e622888591d4b7506
rbgusdlza <rbgusdlza@naver.com>
2024-08-01 16:50:51 +0900

    feat: PostService 생성 및 등록 기능 구현

--- src/main/java/rbgusdlza/petpals/web/service/post/PostService.java
+++ src/main/java/rbgusdlza/petpals/web/service/post/PostService.java
@@ -1,2 +1,25 @@
-package rbgusdlza.petpals.web.service.post;public class PostService {
+package rbgusdlza.petpals.web.service.post;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import rbgusdlza.petpals.domain.post.Post;
+import rbgusdlza.petpals.domain.post.PostRepository;
+import rbgusdlza.petpals.web.service.post.request.PostRegisterServiceRequest;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long register(PostRegisterServiceRequest request) {
+        Post post = request.toEntity();
+        postRepository.save(post);
+        return post.getId();
+    }
 }
