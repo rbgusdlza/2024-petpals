10adf57977d514d190983349b3092d6a16348ccc
rbgusdlza <rbgusdlza@naver.com>
2024-08-01 15:00:50 +0900

    test: Post 테스트 추가

--- src/test/java/rbgusdlza/petpals/domain/post/PostTest.java
+++ src/test/java/rbgusdlza/petpals/domain/post/PostTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.domain.post;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
 class PostTest {
-  
+
+    @DisplayName("게시물의 제목을 수정한다.")
+    @Test
+    void updateTitle() {
+        //given
+        Post post = Post.of(1L, "title1", "content");
+
+        //when
+        post.updateTitle("title2");
+
+        //then
+        assertThat(post.getTitle()).isEqualTo("title2");
+    }
+
+    @DisplayName("게시물의 내용을 수정한다.")
+    @Test
+    void updateContent() {
+        //given
+        Post post = Post.of(1L, "title", "content1");
+
+        //when
+        post.updateContent("content2");
+
+        //then
+        assertThat(post.getContent()).isEqualTo("content2");
+    }
 }
