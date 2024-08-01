4bd11b5b7d34c6068de3ba45aad4c8399597950b
rbgusdlza <rbgusdlza@naver.com>
2024-08-01 16:47:32 +0900

    feat: PostController 생성 및 등록 기능 구현

--- src/main/java/rbgusdlza/petpals/web/controller/post/PostController.java
+++ src/main/java/rbgusdlza/petpals/web/controller/post/PostController.java
@@ -1,2 +1,48 @@
-package rbgusdlza.petpals.web.controller.post;public class PostController {
+package rbgusdlza.petpals.web.controller.post;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import rbgusdlza.petpals.web.controller.post.request.PostRegisterRequest;
+import rbgusdlza.petpals.web.service.post.PostService;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/post")
+@Controller
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("postRegisterRequest", new PostRegisterRequest());
+        return "post/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute PostRegisterRequest request,
+                           BindingResult bindingResult,
+                           HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult : {}", bindingResult);
+            return "post/register";
+        }
+
+        Long memberId = getMemberIdFrom(session);
+        postService.register(request.toServiceRequest(memberId));
+        return "redirect:/";
+    }
+
+    private Long getMemberIdFrom(HttpSession session) {
+        return (Long) session.getAttribute("id");
+    }
 }
