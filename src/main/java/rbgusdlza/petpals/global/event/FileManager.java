3f3103bca2e2e2ac650a19fc5c07ad2e1b685e29
rbgusdlza <rbgusdlza@naver.com>
2024-08-10 20:13:14 +0900

    test: PostService 테스트 구현

---
+++ src/main/java/rbgusdlza/petpals/global/event/FileManager.java
@@ -0,0 +1,2 @@
+package rbgusdlza.petpals.global.event;public class FileManager {
+}
--- src/test/java/rbgusdlza/petpals/web/service/post/PostServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/post/PostServiceTest.java
@@ -1,6 +1,5 @@
 package rbgusdlza.petpals.web.service.post;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
