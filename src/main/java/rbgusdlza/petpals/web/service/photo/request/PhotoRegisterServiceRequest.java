6bb0a12f0a37bf97ea2798ab32ad5beb2fd3d801
rbgusdlza <rbgusdlza@naver.com>
2024-08-15 18:36:00 +0900

    feat: PhotoRegisterServiceRequest 빌더, 정적 팩토리 메서드 추가

--- src/main/java/rbgusdlza/petpals/web/service/photo/request/PhotoRegisterServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/photo/request/PhotoRegisterServiceRequest.java
@@ -1,11 +1,24 @@
 package rbgusdlza.petpals.web.service.photo.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
 public class PhotoRegisterServiceRequest {
 
+    private MultipartFile imageFile;
 
+    @Builder
+    private PhotoRegisterServiceRequest(MultipartFile imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public static PhotoRegisterServiceRequest of(MultipartFile imageFile) {
+        return PhotoRegisterServiceRequest.builder()
+                .imageFile(imageFile)
+                .build();
+    }
 }
