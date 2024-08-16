2ec458976906dcd29937fe35623f32cdd9c23a07
rbgusdlza <rbgusdlza@naver.com>
2024-08-16 18:54:39 +0900

    feat: 파일 저장 이름을 생성하는 클래스 생성

--- src/main/java/rbgusdlza/petpals/global/util/FileNameGenerator.java
+++ src/main/java/rbgusdlza/petpals/global/util/FileNameGenerator.java
@@ -1,2 +1,27 @@
-package rbgusdlza.petpals.global.util;public class FileNameGenerator {
+package rbgusdlza.petpals.global.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class FileNameGenerator {
+
+    private static final String FILE_EXTENSION_SEPARATOR = ".";
+
+    public static String generateStoreFileNameFrom(String originalFileName) {
+        String fileExtension = extractFileExtension(originalFileName);
+        String uniqueFileName = generateUniqueFileName();
+        return uniqueFileName + FILE_EXTENSION_SEPARATOR + fileExtension;
+    }
+
+    private static String generateUniqueFileName() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static String extractFileExtension(String originalFileName) {
+        int fileExtensionStartIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
+        return originalFileName.substring(fileExtensionStartIndex + 1);
+    }
 }
