2e5829c2ebf0ca6ebf09526f37ce6aad885aeb93
rbgusdlza <rbgusdlza@naver.com>
2024-08-16 18:55:39 +0900

    feat: Photo, PhotoDetails를 반환하는 클래스 생성

--- src/main/java/rbgusdlza/petpals/domain/photo/PhotoProcessor.java
+++ src/main/java/rbgusdlza/petpals/domain/photo/PhotoProcessor.java
@@ -1,2 +1,17 @@
-package rbgusdlza.petpals.domain.photo;public class PhotoProcessor {
+package rbgusdlza.petpals.domain.photo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class PhotoProcessor {
+
+    public Photo getPhoto(Long postId, MultipartFile imageFile) {
+        String originalFileName = imageFile.getOriginalFilename();
+        return Photo.of(postId, originalFileName);
+    }
+
+    public PhotoDetails getPhotoDetails(Long photoId, String uniqueFileName) {
+        return PhotoDetails.of(photoId, uniqueFileName);
+    }
 }
