18b54958ef47c4a3c9d61438d1a1583db47a3d89
rbgusdlza <rbgusdlza@naver.com>
2024-08-17 19:15:03 +0900

    test: PhotoService 메서드 테스트 구현

--- src/test/java/rbgusdlza/petpals/web/service/photo/PhotoServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/photo/PhotoServiceTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.web.service.photo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import rbgusdlza.petpals.domain.photo.PhotoHandler;
+import rbgusdlza.petpals.web.service.photo.request.PhotoRegisterServiceRequest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class PhotoServiceTest {
-  
+
+    @Autowired
+    private PhotoService photoService;
+
+    @MockBean
+    private PhotoHandler photoHandler;
+
+    @DisplayName("사진을 등록한다.")
+    @Test
+    void register() {
+        //given
+        MockMultipartFile file = getFile("image.png");
+        PhotoRegisterServiceRequest request = PhotoRegisterServiceRequest.of(1L, file);
+        doNothing().when(photoHandler).saveImageFile(file, "hello.png");
+
+        //when
+        Long photoId = photoService.register(request);
+
+        //then
+        assertThat(photoId).isNotNull();
+    }
+
+    private MockMultipartFile getFile(String originalFileName) {
+        return new MockMultipartFile("file", originalFileName, "image/jpeg", "Hello, World!".getBytes());
+    }
 }
