14f195eb9e11a96dc5dd99ad49936a6dbbf43e01
rbgusdlza <rbgusdlza@naver.com>
2024-08-20 18:49:59 +0900

    feat: PopularityService 생성 및 엔티티 생성 메서드 추가

--- src/main/java/rbgusdlza/petpals/web/service/popularity/PopularityService.java
+++ src/main/java/rbgusdlza/petpals/web/service/popularity/PopularityService.java
@@ -1,2 +1,24 @@
-package rbgusdlza.petpals.web.service.popularity;public class PopularityService {
+package rbgusdlza.petpals.web.service.popularity;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import rbgusdlza.petpals.domain.popularity.Popularity;
+import rbgusdlza.petpals.domain.popularity.PopularityRepository;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class PopularityService {
+
+    private final PopularityRepository popularityRepository;
+
+    @Transactional
+    public Long createFrom(Long postId) {
+        Popularity popularity = Popularity.of(postId, 0.0);
+        popularityRepository.save(popularity);
+        return popularity.getId();
+    }
 }
