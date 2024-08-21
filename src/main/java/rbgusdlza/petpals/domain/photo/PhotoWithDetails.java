c5bc565250556cd3b40b7719a38034f34e3a2b98
rbgusdlza <rbgusdlza@naver.com>
2024-08-21 23:19:38 +0900

    feat: 빌더 및 생성자 생성

--- src/main/java/rbgusdlza/petpals/domain/photo/PhotoWithDetails.java
+++ src/main/java/rbgusdlza/petpals/domain/photo/PhotoWithDetails.java
@@ -1,5 +1,6 @@
 package rbgusdlza.petpals.domain.photo;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,17 @@ public class PhotoWithDetails {
 
     private Photo photo;
     private PhotoDetails photoDetails;
+
+    @Builder
+    private PhotoWithDetails(Photo photo, PhotoDetails photoDetails) {
+        this.photo = photo;
+        this.photoDetails = photoDetails;
+    }
+
+    public static PhotoWithDetails of(Photo photo, PhotoDetails photoDetails) {
+        return PhotoWithDetails.builder()
+                .photo(photo)
+                .photoDetails(photoDetails)
+                .build();
+    }
 }
