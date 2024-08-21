f4808335c15d4a54db9cee300ded81872a8e4fe4
rbgusdlza <rbgusdlza@naver.com>
2024-08-21 23:22:48 +0900

    feat: ErrorCode에 에러 추가

--- src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
+++ src/main/java/rbgusdlza/petpals/web/error/ErrorCode.java
@@ -13,6 +13,9 @@ public enum ErrorCode {
     SENDING_EMAIL_ERROR("이메일 전송 실패"),
     MEMBER_LOGIN_ERROR("매칭되는 사용자 정보 없음"),
     MEMBER_JOIN_ERROR("입력값과 중복된 사용자가 이미 존재"),
+    FINDING_POST_ERROR("매칭되는 게시물 없음"),
+    FINDING_PHOTO_ERROR("매칭되는 사진 없음"),
+    FINDING_PHOTO_DETAILS_ERROR("매칭되는 사진 정보 없음"),
     ;
 
     private final String description;
