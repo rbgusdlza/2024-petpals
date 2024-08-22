0e0b10718ab0f39fc06431464d66445d51a0a257
rbgusdlza <rbgusdlza@naver.com>
2024-08-22 20:23:38 +0900

    feat: findByPhotoId 메서드 추가

--- src/main/java/rbgusdlza/petpals/domain/photo/PhotoDetailsRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/photo/PhotoDetailsRepository.java
@@ -2,5 +2,9 @@ package rbgusdlza.petpals.domain.photo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PhotoDetailsRepository extends JpaRepository<PhotoDetails, Long> {
+
+    Optional<PhotoDetails> findByPhotoId(Long photoId);
 }
