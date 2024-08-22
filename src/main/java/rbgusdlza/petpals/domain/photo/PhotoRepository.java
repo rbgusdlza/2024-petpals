a80a55fa11de87b5479b41c8a577c658008bd305
rbgusdlza <rbgusdlza@naver.com>
2024-08-22 20:25:30 +0900

    feat: findByPostId 메서드 생성

--- src/main/java/rbgusdlza/petpals/domain/photo/PhotoRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/photo/PhotoRepository.java
@@ -2,5 +2,10 @@ package rbgusdlza.petpals.domain.photo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    Optional<Photo> findByPostId(Long postId);
+
 }
