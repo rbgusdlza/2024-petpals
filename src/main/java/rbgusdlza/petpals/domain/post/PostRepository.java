bf761e34691ecd939be10258db061715eb98693d
rbgusdlza <rbgusdlza@naver.com>
2024-08-27 18:06:34 +0900

    feat: PostRepository 메서드 추가 및 테스트 추가

--- src/main/java/rbgusdlza/petpals/domain/post/PostRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/post/PostRepository.java
@@ -1,6 +1,17 @@
 package rbgusdlza.petpals.domain.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query(value = "SELECT * FROM Post p ORDER BY p.post_id DESC LIMIT :limit", nativeQuery = true)
+    List<Post> findAllByOrderByIdDesc(@Param("limit") int limit);
+
+    @Query(value = "SELECT * FROM Post p WHERE p.post_id < :postId ORDER BY p.post_id DESC LIMIT :limit", nativeQuery = true)
+    List<Post> findByIdLessThanOrderByIdDesc(@Param("postId") Long postId, @Param("limit") int limit);
+
 }
--- src/test/java/rbgusdlza/petpals/domain/post/PostRepositoryTest.java
+++ src/test/java/rbgusdlza/petpals/domain/post/PostRepositoryTest.java
@@ -1,4 +1,89 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.domain.post;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class PostRepositoryTest {
-  
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @AfterEach
+    void tearDown() {
+        postRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("Id 기준으로 내림차순된 게시물을 limit만큼 조회한다.")
+    @Test
+    void findAllByOrderByIdDesc() {
+        //given
+        Post post1 = Post.of(1L, "title1", "content1");
+        Post post2 = Post.of(2L, "title2", "content2");
+        Post post3 = Post.of(3L, "title3", "content3");
+        Post post4 = Post.of(4L, "title4", "content4");
+        Post post5 = Post.of(5L, "title5", "content5");
+        postRepository.saveAll(List.of(post1, post2, post3, post4, post5));
+
+        //when
+        List<Post> findPosts = postRepository.findAllByOrderByIdDesc(2);
+
+        //then
+        assertThat(findPosts).hasSize(2)
+                .extracting("title", "content")
+                .containsExactlyInAnyOrder(
+                        tuple("title5", "content5"),
+                        tuple("title4", "content4")
+                );
+    }
+
+    @DisplayName("주어진 Id보다 작은 Id 기준으로 내림차순된 게시물을 최대 limit만큼 조회한다.")
+    @Test
+    void findByIdLessThanOrderByIdDesc() {
+        //given
+        Post post1 = Post.of(1L, "title1", "content1");
+        Post post2 = Post.of(2L, "title2", "content2");
+        Post post3 = Post.of(3L, "title3", "content3");
+        Post post4 = Post.of(4L, "title4", "content4");
+        Post post5 = Post.of(5L, "title5", "content5");
+        postRepository.saveAll(List.of(post1, post2, post3, post4, post5));
+
+        //when
+        List<Post> findPosts = postRepository.findByIdLessThanOrderByIdDesc(3L, 3);
+
+        //then
+        assertThat(findPosts).hasSize(2)
+                .extracting("title", "content")
+                .containsExactlyInAnyOrder(
+                        tuple("title2", "content2"),
+                        tuple("title1", "content1")
+                );
+    }
+
+    @DisplayName("주어진 Id보다 작은 Id 기준으로 내림차순된 게시물을 limit만큼 조회할 때, 해당하는 게시물이 없으면 빈 리스트를 반환한다.")
+    @Test
+    void findByIdLessThanOrderByIdDesc2() {
+        //given
+        Post post1 = Post.of(1L, "title1", "content1");
+        Post post2 = Post.of(2L, "title2", "content2");
+        Post post3 = Post.of(3L, "title3", "content3");
+        Post post4 = Post.of(4L, "title4", "content4");
+        Post post5 = Post.of(5L, "title5", "content5");
+        postRepository.saveAll(List.of(post1, post2, post3, post4, post5));
+
+        //when
+        List<Post> findPosts = postRepository.findByIdLessThanOrderByIdDesc(1L, 3);
+
+        //then
+        assertThat(findPosts).isEmpty();
+    }
 }
