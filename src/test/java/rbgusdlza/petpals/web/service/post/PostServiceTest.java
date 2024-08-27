518a51926d59e2d3f765bb4547797257dafd9015
rbgusdlza <rbgusdlza@naver.com>
2024-08-27 19:11:12 +0900

    test: findBy 메서드 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/service/post/PostServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/post/PostServiceTest.java
@@ -7,10 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
+import rbgusdlza.petpals.domain.member.MemberRepository;
+import rbgusdlza.petpals.domain.photo.Photo;
+import rbgusdlza.petpals.domain.photo.PhotoDetails;
+import rbgusdlza.petpals.domain.photo.PhotoDetailsRepository;
+import rbgusdlza.petpals.domain.photo.PhotoRepository;
+import rbgusdlza.petpals.domain.post.Post;
 import rbgusdlza.petpals.domain.post.PostRepository;
+import rbgusdlza.petpals.domain.reaction.Reaction;
+import rbgusdlza.petpals.domain.reaction.ReactionRepository;
+import rbgusdlza.petpals.domain.reaction.ReactionType;
 import rbgusdlza.petpals.web.service.post.request.PostRegisterServiceRequest;
+import rbgusdlza.petpals.web.service.post.response.PostResponse;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static rbgusdlza.petpals.domain.reaction.ReactionType.*;
+import static rbgusdlza.petpals.domain.reaction.TargetType.POST;
+
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -22,8 +35,19 @@ class PostServiceTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    @Autowired
+    private PhotoDetailsRepository photoDetailsRepository;
+
+    @Autowired
+    private ReactionRepository reactionRepository;
+
     @AfterEach
     void tearDown() {
+        photoDetailsRepository.deleteAllInBatch();
+        photoRepository.deleteAllInBatch();
         postRepository.deleteAllInBatch();
     }
 
@@ -39,6 +63,33 @@ class PostServiceTest {
 
         //then
         assertThat(postId).isNotNull();
+    }
+
+    @DisplayName("게시물 id로 게시물을 조회한다.")
+    @Test
+    void findBy() {
+        //given
+        Post post = Post.of(1L, "hello", "world");
+        postRepository.save(post);
+        Long postId = post.getId();
+
+        Photo photo = Photo.of(postId, "photoName");
+        photoRepository.save(photo);
+        Long photoId = photo.getId();
+
+        PhotoDetails photoDetails = PhotoDetails.of(photoId, "image.png");
+        photoDetailsRepository.save(photoDetails);
+
+        Reaction reaction = Reaction.of(2L, postId, POST, LIKE);
+        reactionRepository.save(reaction);
+
+        //when
+        PostResponse response = postService.findBy(postId);
+
+        //then
+        assertThat(response).isNotNull()
+                .extracting("id", "title", "content", "uploadFileName", "storeFileName", "likeCount")
+                .containsExactly(postId, "hello", "world", "photoName", "image.png", 1L);
     }
 
     private MockMultipartFile getFile(String originalFileName) {
