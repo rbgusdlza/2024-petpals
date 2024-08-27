64d0b02a1cc6c1a39cedc462e2641d40b3d028cf
rbgusdlza <rbgusdlza@naver.com>
2024-08-27 19:12:11 +0900

    test: create 메서드 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/service/popularity/PopularityServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/popularity/PopularityServiceTest.java
@@ -1,4 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.web.service.popularity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import rbgusdlza.petpals.domain.popularity.PopularityRepository;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class PopularityServiceTest {
-  
+
+    @Autowired
+    private PopularityService popularityService;
+
+    @Autowired
+    private PopularityRepository popularityRepository;
+
+    @AfterEach
+    void tearDown() {
+        popularityRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("인기도를 생성한다.")
+    @Test
+    void createPopularity() {
+        //given
+        Long postId = 1L;
+
+        //when
+        Long popularityId = popularityService.createPopularity(postId);
+
+        //then
+        assertThat(popularityId).isNotNull();
+    }
 }
