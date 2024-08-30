8a91334611ed73ce1e3084fc06012c7c130b1698
rbgusdlza <rbgusdlza@naver.com>
2024-08-30 18:35:00 +0900

    feat: PostResponse 생성 및 정적 팩토리 메서드 구현

--- src/main/java/rbgusdlza/petpals/web/service/post/response/PostResponse.java
+++ src/main/java/rbgusdlza/petpals/web/service/post/response/PostResponse.java
@@ -1,2 +1,38 @@
-package rbgusdlza.petpals.web.service.post.response;public class PostResponse {
+package rbgusdlza.petpals.web.service.post.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import rbgusdlza.petpals.domain.photo.PhotoWithDetails;
+import rbgusdlza.petpals.domain.post.Post;
+
+@Getter
+public class PostResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String uploadFileName;
+    private String storeFileName;
+    private long likeCount;
+
+    @Builder
+    private PostResponse(Long id, String title, String content, String uploadFileName, String storeFileName, long likeCount) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+        this.likeCount = likeCount;
+    }
+
+    public static PostResponse of(Post post, PhotoWithDetails photoWithDetails, long likeCount) {
+        return PostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .uploadFileName(photoWithDetails.getPhoto().getPhotoName())
+                .storeFileName(photoWithDetails.getPhotoDetails().getStoreFileName())
+                .likeCount(likeCount)
+                .build();
+    }
 }
