b35fd60b9fe53a0cb8d9735cb19e9c1bbb8e1af7
rbgusdlza <rbgusdlza@naver.com>
2024-08-30 18:35:41 +0900

    feat: 클래스 이름 변경 및 countlike 메서드 추가

--- src/main/java/rbgusdlza/petpals/web/service/reaction/LikeService.java
+++ src/main/java/rbgusdlza/petpals/web/service/reaction/LikeService.java
@@ -5,14 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rbgusdlza.petpals.domain.reaction.ReactionRepository;
+import rbgusdlza.petpals.domain.reaction.ReactionType;
+import rbgusdlza.petpals.domain.reaction.TargetType;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
-public class ReactionService {
+public class LikeService {
 
     private final ReactionRepository reactionRepository;
 
-    public long
+    public long countLike(Long targetId, TargetType targetType) {
+        return reactionRepository.countByTargetIdAndTargetTypeAndType(targetId, targetType, ReactionType.LIKE);
+    }
 }
