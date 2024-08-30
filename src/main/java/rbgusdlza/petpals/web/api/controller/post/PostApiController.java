f90b2b92b115374d29c26c9eea653a5d01a9a60b
rbgusdlza <rbgusdlza@naver.com>
2024-08-30 18:03:16 +0900

    feat: list 메서드 추가

--- src/main/java/rbgusdlza/petpals/web/api/controller/post/PostApiController.java
+++ src/main/java/rbgusdlza/petpals/web/api/controller/post/PostApiController.java
@@ -1,2 +1,27 @@
-package rbgusdlza.petpals.web.api.controller.post;public class PostApiController {
+package rbgusdlza.petpals.web.api.controller.post;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import rbgusdlza.petpals.web.api.ApiResponse;
+import rbgusdlza.petpals.web.api.controller.post.request.PostCursorRequest;
+import rbgusdlza.petpals.web.api.controller.post.response.SliceResult;
+import rbgusdlza.petpals.web.service.post.PostService;
+import rbgusdlza.petpals.web.service.post.response.PostResponse;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+@RestController
+public class PostApiController {
+
+    private final PostService postService;
+
+    @PostMapping("/list")
+    public ApiResponse<SliceResult> list(@RequestBody PostCursorRequest request) {
+        List<PostResponse> posts = postService.findAfter(request.toServiceRequest());
+        return ApiResponse.ok(SliceResult.of(posts, request.getLimit()));
+    }
 }
