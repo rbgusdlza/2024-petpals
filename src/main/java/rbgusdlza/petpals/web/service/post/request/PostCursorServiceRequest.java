9df02618d0fe9fc1ee80fab0a5e917f17a8d78a5
rbgusdlza <rbgusdlza@naver.com>
2024-08-30 18:33:37 +0900

    feat: PostCursorServiceRequest 생성 및 정적 팩토리 메서드 구현

--- src/main/java/rbgusdlza/petpals/web/service/post/request/PostCursorServiceRequest.java
+++ src/main/java/rbgusdlza/petpals/web/service/post/request/PostCursorServiceRequest.java
@@ -1,2 +1,26 @@
-package rbgusdlza.petpals.web.service.post.request;public class PostCursorServiceRequest {
+package rbgusdlza.petpals.web.service.post.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostCursorServiceRequest {
+
+    private Long targetId;
+    private int limit;
+
+    @Builder
+    private PostCursorServiceRequest(Long targetId, int limit) {
+        this.targetId = targetId;
+        this.limit = limit;
+    }
+
+    public static PostCursorServiceRequest of(Long targetId, int limit) {
+        return PostCursorServiceRequest.builder()
+                .targetId(targetId)
+                .limit(limit)
+                .build();
+    }
 }
