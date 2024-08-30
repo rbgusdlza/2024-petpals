fa0fe0b434f7b65ed33422f43a03bf5e5fa736e9
rbgusdlza <rbgusdlza@naver.com>
2024-08-31 00:56:30 +0900

    feat: 좋아요 관련 api controller 생성 및 메서드 구현

--- src/main/java/rbgusdlza/petpals/web/api/controller/reaction/LikeApiController.java
+++ src/main/java/rbgusdlza/petpals/web/api/controller/reaction/LikeApiController.java
@@ -1,2 +1,38 @@
-package rbgusdlza.petpals.web.api.controller.reaction;public class LikeApiController {
+package rbgusdlza.petpals.web.api.controller.reaction;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rbgusdlza.petpals.web.api.ApiResponse;
+import rbgusdlza.petpals.web.service.reaction.LikeService;
+
+import static rbgusdlza.petpals.domain.reaction.TargetType.POST;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+@RestController
+public class LikeApiController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/{postId}/like")
+    public ApiResponse<Long> like(@PathVariable Long postId,
+                                  HttpSession session) {
+        Long memberId = getMemberIdFrom(session);
+        return ApiResponse.ok(likeService.like(memberId, postId, POST));
+    }
+
+    @PostMapping("/{postId}/count-like")
+    public ApiResponse<Long> countLike(@PathVariable Long postId) {
+        return ApiResponse.ok(likeService.countLike(postId, POST));
+    }
+
+    private Long getMemberIdFrom(HttpSession session) {
+        return (Long) session.getAttribute("id");
+    }
 }
