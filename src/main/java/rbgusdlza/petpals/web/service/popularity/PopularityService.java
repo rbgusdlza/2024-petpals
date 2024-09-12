63f00ec3620641b432f065e974a2c17f3af3f936
rbgusdlza <rbgusdlza@naver.com>
2024-09-12 15:43:09 +0900

    feat: 인기도를 업데이트하는 메서드 구현

--- src/main/java/rbgusdlza/petpals/web/service/popularity/PopularityService.java
+++ src/main/java/rbgusdlza/petpals/web/service/popularity/PopularityService.java
@@ -6,6 +6,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rbgusdlza.petpals.domain.popularity.Popularity;
 import rbgusdlza.petpals.domain.popularity.PopularityRepository;
+import rbgusdlza.petpals.web.error.PetPalsException;
+import rbgusdlza.petpals.web.service.reaction.LikeService;
+
+import java.util.Optional;
+
+import static rbgusdlza.petpals.domain.reaction.TargetType.*;
+import static rbgusdlza.petpals.web.error.ErrorCode.POPULARITY_NOT_FOUND;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -14,11 +21,21 @@ import rbgusdlza.petpals.domain.popularity.PopularityRepository;
 public class PopularityService {
 
     private final PopularityRepository popularityRepository;
+    private final LikeService likeService;
 
     @Transactional
     public Long create(Long postId) {
         Popularity popularity = Popularity.of(postId, 0.0);
         popularityRepository.save(popularity);
+        return popularity.getId();
+    }
+
+    @Transactional
+    public Long update(Long postId) {
+        Popularity popularity = popularityRepository.findByPostId(postId)
+                .orElseThrow(() -> new PetPalsException(POPULARITY_NOT_FOUND));
+        long likeCount = likeService.countLike(postId, POST);
+        popularity.updateScore(likeCount);
         return popularity.getId();
     }
 }
