caf082a6337cd25137275fc88839969da9716c19
rbgusdlza <rbgusdlza@naver.com>
2024-09-12 19:18:43 +0900

    feat: 인기도 점수가 높은 순으로 입력된 수만큼 인기도를 반환하는 메서드 추가

--- src/main/java/rbgusdlza/petpals/domain/popularity/PopularityRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/popularity/PopularityRepository.java
@@ -1,10 +1,17 @@
 package rbgusdlza.petpals.domain.popularity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PopularityRepository extends JpaRepository<Popularity, Long> {
 
     Optional<Popularity> findByPostId(Long postId);
+
+    @Query(value = "SELECT * FROM Popularity p ORDER BY p.score DESC LIMIT :limit", nativeQuery = true)
+    List<Popularity> findAllByOrderByScoreDesc(@Param("limit") int limit);
+
 }
