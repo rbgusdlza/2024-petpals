cb97fbc696e69e02a603679e65157e06fd967c39
rbgusdlza <rbgusdlza@naver.com>
2024-09-12 19:21:02 +0900

    feat: PopularityResponse 추가

--- src/main/java/rbgusdlza/petpals/web/service/popularity/response/PopularityResponse.java
+++ src/main/java/rbgusdlza/petpals/web/service/popularity/response/PopularityResponse.java
@@ -1,2 +1,25 @@
-package rbgusdlza.petpals.web.service.popularity.response;public class PopularityResponse {
+package rbgusdlza.petpals.web.service.popularity.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import rbgusdlza.petpals.domain.popularity.Popularity;
+
+@Getter
+public class PopularityResponse {
+
+    private Long postId;
+    private double score;
+
+    @Builder
+    private PopularityResponse(Long postId, double score) {
+        this.postId = postId;
+        this.score = score;
+    }
+
+    public static PopularityResponse of(Popularity popularity) {
+        return PopularityResponse.builder()
+                .postId(popularity.getPostId())
+                .score(popularity.getScore())
+                .build();
+    }
 }
