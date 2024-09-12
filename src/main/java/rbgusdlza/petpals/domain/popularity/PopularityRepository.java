58d71945b43ccd3a2b17a01cfd1da8deb5d8a9b6
rbgusdlza <rbgusdlza@naver.com>
2024-09-12 15:42:38 +0900

    feat: postId로 인기도를 찾는 메서드 추가

--- src/main/java/rbgusdlza/petpals/domain/popularity/PopularityRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/popularity/PopularityRepository.java
@@ -2,5 +2,9 @@ package rbgusdlza.petpals.domain.popularity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PopularityRepository extends JpaRepository<Popularity, Long> {
+
+    Optional<Popularity> findByPostId(Long postId);
 }
