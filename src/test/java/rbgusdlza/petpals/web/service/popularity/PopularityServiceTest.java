00998f05cf012167af2e08348da49f395f509482
rbgusdlza <rbgusdlza@naver.com>
2024-09-13 20:15:38 +0900

    test: 인기도 업데이트, 점수 순에 따라 인기도를 조회하는 메서드 테스트 구현

--- src/test/java/rbgusdlza/petpals/web/service/popularity/PopularityServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/popularity/PopularityServiceTest.java
@@ -1,18 +1,30 @@
 package rbgusdlza.petpals.web.service.popularity;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import rbgusdlza.petpals.domain.popularity.Popularity;
 import rbgusdlza.petpals.domain.popularity.PopularityRepository;
+import rbgusdlza.petpals.domain.reaction.Reaction;
+import rbgusdlza.petpals.domain.reaction.ReactionRepository;
+import rbgusdlza.petpals.web.service.popularity.response.PopularityResponse;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static rbgusdlza.petpals.domain.reaction.ReactionType.*;
+import static rbgusdlza.petpals.domain.reaction.TargetType.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class PopularityServiceTest {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private PopularityService popularityService;
@@ -20,14 +32,18 @@ class PopularityServiceTest {
     @Autowired
     private PopularityRepository popularityRepository;
 
+    @Autowired
+    private ReactionRepository reactionRepository;
+
     @AfterEach
     void tearDown() {
         popularityRepository.deleteAllInBatch();
+        reactionRepository.deleteAllInBatch();
     }
 
     @DisplayName("인기도를 생성한다.")
     @Test
-    void createPopularity() {
+    void create() {
         //given
         Long postId = 1L;
 
@@ -36,5 +52,48 @@ class PopularityServiceTest {
 
         //then
         assertThat(popularityId).isNotNull();
+    }
+
+    @DisplayName("인기도를 업데이트한다.")
+    @Test
+    void update() {
+        //given
+        Long postId = 1L;
+
+        Reaction reaction = Reaction.of(1L, postId, POST, LIKE);
+        reactionRepository.save(reaction);
+
+        Popularity popularity = Popularity.of(postId, 0.0);
+        popularityRepository.save(popularity);
+
+        //when
+        popularityService.update(postId);
+
+        //then
+        Popularity findPopularity = popularityRepository.findByPostId(postId).get();
+        assertThat(findPopularity).isNotNull()
+                .extracting("score")
+                .isEqualTo(0.1);
+    }
+
+    @DisplayName("점수가 높은 순서대로 limit만큼 인기도를 조회한다.")
+    @Test
+    void find() {
+        //given
+        Popularity popularity1 = Popularity.of(1L, 0.0);
+        Popularity popularity2 = Popularity.of(2L, 2.0);
+        Popularity popularity3 = Popularity.of(3L, 5.0);
+        popularityRepository.saveAll(List.of(popularity1, popularity2, popularity3));
+
+        //when
+        List<PopularityResponse> response = popularityService.find(2);
+
+        //then
+        assertThat(response).hasSize(2)
+                .extracting("postId", "score")
+                .containsExactlyInAnyOrder(
+                        tuple(3L, 5.0),
+                        tuple(2L, 2.0)
+                );
     }
 }
