ec25c422ce487bf53b40b8877ece83828114a8f4
rbgusdlza <rbgusdlza@naver.com>
2024-09-17 01:22:35 +0900

    feat: Rabbitmq 설정 정보 추가

--- src/main/java/rbgusdlza/petpals/global/config/RabbitmqConfig.java
+++ src/main/java/rbgusdlza/petpals/global/config/RabbitmqConfig.java
@@ -1,2 +1,47 @@
-package rbgusdlza.petpals.global.config;public class RabbitmqConfig {
+package rbgusdlza.petpals.global.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    private static final String EXCHANGE_NAME = "like.exchange";
+    private static final String QUEUE_NAME = "like.queue";
+    private static final String ROUTING_KEY = "like.routing";
+
+    @Bean
+    DirectExchange directExchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    Binding binding(DirectExchange directExchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(directExchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
 }
