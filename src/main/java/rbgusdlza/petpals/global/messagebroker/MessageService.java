a52eb2fbe1748ef3add3f3e9d924b460311a3ffe
rbgusdlza <rbgusdlza@naver.com>
2024-09-17 01:23:04 +0900

    feat: 메시지 브로커 인터페이스 생성

--- src/main/java/rbgusdlza/petpals/global/messagebroker/MessageService.java
+++ src/main/java/rbgusdlza/petpals/global/messagebroker/MessageService.java
@@ -1,4 +1,4 @@
-package rbgusdlza.petpals.global.message;
+package rbgusdlza.petpals.global.messagebroker;
 
 public interface MessageService {
     void sendMessage(String exchange, String routingKey, Object message);
