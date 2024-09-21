201ef8d148ced114374fd5058026f1b54ff328cf
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:34:31 +0900

    test: PhotoRepositoryTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/domain/photo/PhotoRepositoryTest.java
+++ src/test/java/rbgusdlza/petpals/domain/photo/PhotoRepositoryTest.java
@@ -1,4 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.domain.photo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class PhotoRepositoryTest {
-  
+
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    @AfterEach
+    void tearDown() {
+        photoRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("게시물 아이디로 사진을 조회한다.")
+    @Test
+    void findByPostId() {
+        //given
+        Photo photo = Photo.of(1L, "photo");
+        photoRepository.save(photo);
+
+        //when
+        Photo findPhoto = photoRepository.findByPostId(1L).get();
+
+        //then
+        assertThat(findPhoto).isNotNull()
+                .extracting("postId", "photoName")
+                .containsExactlyInAnyOrder(1L, "photo");
+    }
 }
