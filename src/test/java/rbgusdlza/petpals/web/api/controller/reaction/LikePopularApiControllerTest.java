ab47a99c07f9f1c6403a90d53c88812cb7012a27
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:37:02 +0900

    test: LikePopularApiControllerTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/api/controller/reaction/LikePopularApiControllerTest.java
+++ src/test/java/rbgusdlza/petpals/web/api/controller/reaction/LikePopularApiControllerTest.java
@@ -1,4 +1,55 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.web.api.controller.reaction;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import rbgusdlza.petpals.global.messagebroker.MessageService;
+import rbgusdlza.petpals.web.service.reaction.LikeCachedService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LikePopularApiController.class)
 class LikePopularApiControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LikeCachedService likeCachedService;
+
+    @MockBean
+    private MessageService messageService;
+
+    @DisplayName("인기 게시물 아이디로 좋아요를 생성한다.")
+    @Test
+    void like() throws Exception {
+        mockMvc.perform(
+                        post("/api/post/popular/{postId}/like", 1L)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+
+    @DisplayName("인기 게시물 아이디로 좋아요를 생성한다.")
+    @Test
+    void countLike() throws Exception {
+        mockMvc.perform(
+                        get("/api/post/popular/{postId}/count-like", 1L)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
 }
