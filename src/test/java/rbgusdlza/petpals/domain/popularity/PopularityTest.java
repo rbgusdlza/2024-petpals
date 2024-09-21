968a338e00f51b00be0aa3c5467f85fb5f0a2d22
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:35:21 +0900

    test: PopularityTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/domain/popularity/PopularityTest.java
+++ src/test/java/rbgusdlza/petpals/domain/popularity/PopularityTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.domain.popularity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class PopularityTest {
-  
+
+    private static final double EPSILON = 0.00001;
+
+    @DisplayName("점수를 업데이트한다.")
+    @Test
+    void updateScore() {
+        //given
+        Popularity popularity = Popularity.of(1L, 0.5);
+
+        //when
+        popularity.updateScore(3);
+
+        //then
+        assertThat(popularity.getScore()).isCloseTo(0.3, within(EPSILON));
+    }
+
 }
