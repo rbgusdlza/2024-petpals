a6aafbf78c64f3b6e4711ec41e84e8cdbf899208
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:34:54 +0900

    test: PopularityRepositoryTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/domain/popularity/PopularityRepositoryTest.java
+++ src/test/java/rbgusdlza/petpals/domain/popularity/PopularityRepositoryTest.java
@@ -1,4 +1,62 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.domain.popularity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class PopularityRepositoryTest {
-  
+
+    @Autowired
+    private PopularityRepository popularityRepository;
+
+    @AfterEach
+    void tearDown() {
+        popularityRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("게시물 아이디로 인기도를 조회한다.")
+    @Test
+    void findByPostId() {
+        //given
+        Popularity popularity = Popularity.of(1L, 0.5);
+        popularityRepository.save(popularity);
+
+        //when
+        Popularity findPopularity = popularityRepository.findByPostId(1L).get();
+
+        //then
+        assertThat(findPopularity).isNotNull()
+                .extracting("postId", "score")
+                .containsExactlyInAnyOrder(1L, 0.5);
+    }
+
+    @DisplayName("점수가 높은 순서대로 주어진 수만큼 인기도를 조회한다.")
+    @Test
+    void findAllByOrderByScoreDesc() {
+        //given
+        Popularity popularity1 = Popularity.of(1L, 0.1);
+        Popularity popularity2 = Popularity.of(2L, 0.2);
+        Popularity popularity3 = Popularity.of(3L, 0.3);
+        popularityRepository.saveAll(List.of(popularity1, popularity2, popularity3));
+
+        //when
+        List<Popularity> popularities = popularityRepository.findAllByOrderByScoreDesc(2);
+
+        //then
+        assertThat(popularities).hasSize(2)
+                .extracting("postId", "score")
+                .containsExactly(
+                        tuple(3L, 0.3),
+                        tuple(2L, 0.2)
+                );
+    }
 }
