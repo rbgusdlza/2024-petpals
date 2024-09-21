cd2426f589e15478fa501f52c978517c97002d86
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:36:26 +0900

    test: PostApiControllerTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/api/controller/post/PostApiControllerTest.java
+++ src/test/java/rbgusdlza/petpals/web/api/controller/post/PostApiControllerTest.java
@@ -1,4 +1,76 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rbgusdlza.petpals.web.api.controller.post;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import rbgusdlza.petpals.web.api.controller.post.request.PostCursorRequest;
+import rbgusdlza.petpals.web.service.post.PostService;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = PostApiController.class)
 class PostApiControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PostService postService;
+
+    @DisplayName("최근 게시물을 주어진 수만큼 조회한다.")
+    @Test
+    void showLatest() throws Exception {
+        mockMvc.perform(
+                        get("/api/post/latest?limit=5")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+
+    @DisplayName("인기 게시물을 주어진 수만큼 조회한다.")
+    @Test
+    void showPopular() throws Exception {
+        mockMvc.perform(
+                        get("/api/post/popular?limit=5")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+
+    @DisplayName("게시물을 주어진 수만큼 추가로 조회한다.")
+    @Test
+    void list() throws Exception {
+        //given
+        PostCursorRequest request = PostCursorRequest.of(3L, 5);
+
+        //when //then
+        mockMvc.perform(
+                        post("/api/post/list")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
 }
