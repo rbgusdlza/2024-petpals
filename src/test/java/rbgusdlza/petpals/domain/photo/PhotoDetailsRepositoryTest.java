158a82989d0a1c82f739d8cad41b21d19fa911cb
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:34:01 +0900

    test: PhotoDetailsRepositoryTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/domain/photo/PhotoDetailsRepositoryTest.java
+++ src/test/java/rbgusdlza/petpals/domain/photo/PhotoDetailsRepositoryTest.java
@@ -1,2 +1,39 @@
-package rbgusdlza.petpals.domain.photo;public class PhotoDetailsRepositoryTest {
+package rbgusdlza.petpals.domain.photo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class PhotoDetailsRepositoryTest {
+
+    @Autowired
+    private PhotoDetailsRepository photoDetailsRepository;
+
+    @AfterEach
+    void tearDown() {
+        photoDetailsRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("사진 아이디로 사진 정보를 조회한다.")
+    @Test
+    void findByPhotoId() {
+        //given
+        PhotoDetails photoDetails = PhotoDetails.of(1L, "photo.png");
+        photoDetailsRepository.save(photoDetails);
+
+        //when
+        PhotoDetails findPhotoDetails = photoDetailsRepository.findByPhotoId(1L).get();
+
+        //then
+        assertThat(findPhotoDetails).isNotNull()
+                .extracting("photoId", "storeFileName")
+                .containsExactlyInAnyOrder(1L, "photo.png");
+    }
 }
