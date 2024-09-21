13126ca81b0cf2cff4f898b70a99987231dc5b13
rbgusdlza <rbgusdlza@naver.com>
2024-09-22 00:38:37 +0900

    test: PhotoServiceTest 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/service/photo/PhotoServiceTest.java
+++ src/test/java/rbgusdlza/petpals/web/service/photo/PhotoServiceTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-import rbgusdlza.petpals.domain.photo.PhotoHandler;
-import rbgusdlza.petpals.domain.photo.PhotoRepository;
+import rbgusdlza.petpals.domain.photo.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
@@ -24,12 +23,16 @@ class PhotoServiceTest {
     @Autowired
     private PhotoRepository photoRepository;
 
+    @Autowired
+    private PhotoDetailsRepository photoDetailsRepository;
+
     @MockBean
     private PhotoHandler photoHandler;
 
     @AfterEach
     void tearDown() {
         photoRepository.deleteAllInBatch();
+        photoDetailsRepository.deleteAllInBatch();
     }
 
     @DisplayName("사진을 등록한다.")
@@ -44,6 +47,33 @@ class PhotoServiceTest {
 
         //then
         assertThat(photoId).isNotNull();
+    }
+
+    @DisplayName("게시물 아이디로 사진과 사진 정보를 조회한다.")
+    @Test
+    void findPhotoWithDetailsBy() {
+        //given
+        Long postId = 1L;
+        Photo photo = Photo.of(postId, "photo");
+        photoRepository.save(photo);
+        Long photoId = photo.getId();
+
+        PhotoDetails photoDetails = PhotoDetails.of(photoId, "photo.png");
+        photoDetailsRepository.save(photoDetails);
+
+        //when
+        PhotoWithDetails photoWithDetails = photoService.findPhotoWithDetailsBy(postId);
+
+        //then
+        Photo findPhoto = photoWithDetails.getPhoto();
+        assertThat(findPhoto).isNotNull()
+                .extracting("postId", "photoName")
+                .containsExactly(postId, "photo");
+
+        PhotoDetails findPhotoDetails = photoWithDetails.getPhotoDetails();
+        assertThat(findPhotoDetails).isNotNull()
+                .extracting("photoId", "storeFileName")
+                .containsExactly(photoId, "photo.png");
     }
 
     private MockMultipartFile getFile(String originalFileName) {
