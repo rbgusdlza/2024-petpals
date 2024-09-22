1f9785f61e6628193b29cc300f198b29798f3dca
rbgusdlza <rbgusdlza@naver.com>
2024-09-23 02:06:20 +0900

    feat: 파일 저장 경로 s3로 변경

--- src/main/java/rbgusdlza/petpals/domain/photo/PhotoHandler.java
+++ src/main/java/rbgusdlza/petpals/domain/photo/PhotoHandler.java
@@ -1,35 +1,45 @@
 package rbgusdlza.petpals.domain.photo;
 
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Operations;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class PhotoHandler {
 
-    @Value("${file.directory}")
-    private String fileDirectory;
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    private final S3Operations s3Operations;
 
     public void saveImageFile(MultipartFile imageFile, String storeFileName) {
         try {
-            String fullPath = getFullPathFrom(storeFileName);
-            File storeImageFile = createImageFileAt(fullPath);
-            imageFile.transferTo(storeImageFile);
+            checkIfImageFileIsEmpty(imageFile);
+            uploadFileTo(imageFile, storeFileName);
         } catch (IOException e) {
-            log.error("파일 입출력 오류 발생");
+            log.error("파일 입출력 오류 발생", e);
         }
     }
 
-    private String getFullPathFrom(String fileName) {
-        return fileDirectory + fileName;
+    private void checkIfImageFileIsEmpty(MultipartFile imageFile) {
+        if (imageFile.isEmpty()) {
+            throw new IllegalArgumentException("이미지 파일 없음");
+        }
     }
 
-    private File createImageFileAt(String fullPath) {
-        return new File(fullPath);
+    private void uploadFileTo(MultipartFile imageFile, String storeFileName) throws IOException {
+        try (InputStream inputStream = imageFile.getInputStream()) {
+            s3Operations.upload(bucketName, storeFileName, inputStream,
+                    ObjectMetadata.builder().contentType(imageFile.getContentType()).build());
+        }
     }
 }
