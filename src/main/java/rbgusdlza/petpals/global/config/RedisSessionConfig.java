f26fe603c5225e635b275d926e652ea5ae3e0c11
rbgusdlza <rbgusdlza@naver.com>
2024-09-23 17:52:51 +0900

    feat: 세션 관련 redis config 추가

--- src/main/java/rbgusdlza/petpals/global/config/RedisSessionConfig.java
+++ src/main/java/rbgusdlza/petpals/global/config/RedisSessionConfig.java
@@ -1,2 +1,25 @@
-package rbgusdlza.petpals.global.config;public class RedisSessionConfig {
+package rbgusdlza.petpals.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisSessionConfig {
+
+    @Value("${spring.data.redis.session.host}")
+    private String host;
+
+    @Value("${spring.data.redis.session.port}")
+    private int port;
+
+    @Primary
+    @Bean
+    public RedisConnectionFactory redisCacheConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
 }
