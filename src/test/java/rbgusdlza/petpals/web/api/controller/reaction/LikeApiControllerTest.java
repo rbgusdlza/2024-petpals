b4ae6c91b1429dceeab474d49115098db501b2fc
rbgusdlza <rbgusdlza@naver.com>
2024-09-28 18:54:44 +0900

    test: like 메서드 관련 테스트 추가

--- src/test/java/rbgusdlza/petpals/web/api/controller/reaction/LikeApiControllerTest.java
+++ src/test/java/rbgusdlza/petpals/web/api/controller/reaction/LikeApiControllerTest.java
@@ -2,6 +2,7 @@ package rbgusdlza.petpals.web.api.controller.reaction;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpSession;
 import rbgusdlza.petpals.ControllerTestSupport;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +18,7 @@ class LikeApiControllerTest extends ControllerTestSupport {
     void like() throws Exception {
         mockMvc.perform(
                         post("/api/post/{postId}/like", 1L)
+                                .sessionAttr("id", 1L)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -25,7 +27,20 @@ class LikeApiControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.message").value("OK"));
     }
 
-    @DisplayName("게시물 아이디로 좋아요를 생성한다.")
+    @DisplayName("게시물 아이디로 좋아요를 생성할 때, 로그인되어 있지 않으면 fail 응답을 반환한다.")
+    @Test
+    void likeWithFail() throws Exception {
+        mockMvc.perform(
+                        post("/api/post/{postId}/like", 1L)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("로그인 상태에서만 좋아요가 가능합니다."));
+    }
+
+    @DisplayName("좋아요 개수를 조회한다.")
     @Test
     void countLike() throws Exception {
         mockMvc.perform(
--- src/test/java/rbgusdlza/petpals/web/api/controller/reaction/LikePopularApiControllerTest.java
+++ src/test/java/rbgusdlza/petpals/web/api/controller/reaction/LikePopularApiControllerTest.java
@@ -17,6 +17,7 @@ class LikePopularApiControllerTest extends ControllerTestSupport {
     void like() throws Exception {
         mockMvc.perform(
                         post("/api/post/popular/{postId}/like", 1L)
+                                .sessionAttr("id", 1L)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -25,7 +26,20 @@ class LikePopularApiControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.message").value("OK"));
     }
 
-    @DisplayName("인기 게시물 아이디로 좋아요를 생성한다.")
+    @DisplayName("게시물 아이디로 좋아요를 생성할 때, 로그인되어 있지 않으면 fail 응답을 반환한다.")
+    @Test
+    void likeWithFail() throws Exception {
+        mockMvc.perform(
+                        post("/api/post/popular/{postId}/like", 1L)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("로그인 상태에서만 좋아요가 가능합니다."));
+    }
+
+    @DisplayName("인기 게시물의 좋아요 개수를 조회한다.")
     @Test
     void countLike() throws Exception {
         mockMvc.perform(
