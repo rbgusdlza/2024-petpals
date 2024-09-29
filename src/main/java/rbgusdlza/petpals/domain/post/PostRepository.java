358a8f061bef9a7076d607e4bdd6582360fc923f
rbgusdlza <rbgusdlza@naver.com>
2024-09-30 03:43:43 +0900

    fix: Post -> post 로 수정

--- src/main/java/rbgusdlza/petpals/domain/post/PostRepository.java
+++ src/main/java/rbgusdlza/petpals/domain/post/PostRepository.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = "SELECT * FROM Post p WHERE p.entity_status = 'ACTIVE' ORDER BY p.post_id DESC LIMIT :limit", nativeQuery = true)
+    @Query(value = "SELECT * FROM post p WHERE p.entity_status = 'ACTIVE' ORDER BY p.post_id DESC LIMIT :limit", nativeQuery = true)
     List<Post> findAllByOrderByIdDesc(@Param("limit") int limit);
 
-    @Query(value = "SELECT * FROM Post p WHERE p.entity_status = 'ACTIVE' AND p.post_id < :postId ORDER BY p.post_id DESC LIMIT :limit", nativeQuery = true)
+    @Query(value = "SELECT * FROM post p WHERE p.entity_status = 'ACTIVE' AND p.post_id < :postId ORDER BY p.post_id DESC LIMIT :limit", nativeQuery = true)
     List<Post> findAllByIdLessThanOrderByIdDesc(@Param("postId") Long postId, @Param("limit") int limit);
 
 }
