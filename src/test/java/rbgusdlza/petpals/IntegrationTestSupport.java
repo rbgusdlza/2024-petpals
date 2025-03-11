05125400c19b0f72393a4e1fa307dd028bb6e99b
rbgusdlza <rbgusdlza@naver.com>
2025-03-11 16:21:18 +0900

    test: 외부 시스템(redis, rabbitmq) 설정 mock 추가

--- src/test/java/rbgusdlza/petpals/IntegrationTestSupport.java
+++ src/test/java/rbgusdlza/petpals/IntegrationTestSupport.java
@@ -1,18 +1,37 @@
 package rbgusdlza.petpals;
 
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import rbgusdlza.petpals.domain.photo.PhotoHandler;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        RedisAutoConfiguration.class,
+        RedisRepositoriesAutoConfiguration.class,
+        RabbitAutoConfiguration.class
+})
+@TestPropertySource(properties = "spring.session.store-type=none")
 public abstract class IntegrationTestSupport {
 
     @MockBean
-    protected CacheManager cacheManager;
+    protected PhotoHandler photoHandler;
 
     @MockBean
-    protected PhotoHandler photoHandler;
+    protected RabbitTemplate rabbitTemplate;
+
+    @MockBean
+    protected ConnectionFactory connectionFactory;
+
+    @MockBean
+    protected SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory;
 }
