b3087aa3c4091125ec828a2e70f6dacdc1fb9a03
rbgusdlza <rbgusdlza@naver.com>
2025-09-26 20:49:09 +0900

    feat: 좋아요 관련 큐 중복 insert 발생시 생기는 에러는 무시하도록 처리

--- src/main/java/rbgusdlza/petpals/global/messagebroker/LikeCacheUpdateListener.java
+++ src/main/java/rbgusdlza/petpals/global/messagebroker/LikeCacheUpdateListener.java
@@ -3,6 +3,7 @@ package rbgusdlza.petpals.global.messagebroker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import rbgusdlza.petpals.global.messagebroker.request.LikeCacheUpdateMessage;
@@ -18,7 +19,11 @@ public class LikeCacheUpdateListener {
     @Transactional
     @RabbitListener(queues = "like.queue")
     public void handleLikeCacheUpdateMessage(LikeCacheUpdateMessage message) {
-        log.debug("Consume like message: {}", message);
-        likeService.like(message.getMemberId(), message.getTargetId(), message.getTargetType());
+        try {
+            likeService.like(message.getMemberId(), message.getTargetId(), message.getTargetType());
+        } catch (DataIntegrityViolationException e) {
+            log.info("Duplicate like ignored. memberId={}, targetId={}, targetType={}",
+                    message.getMemberId(), message.getTargetId(), message.getTargetType());
+        }
     }
 }
